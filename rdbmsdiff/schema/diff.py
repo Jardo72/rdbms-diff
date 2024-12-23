75df65e1823a71db6e0f1adc2e971fb72fd81dd2
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2024-12-23 18:55:39 +0100

    Further functionality added.

--- rdbmsdiff/schema/diff.py
+++ rdbmsdiff/schema/diff.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Any, Optional, Tuple
+from typing import Any, Optional, Sequence, Set, Tuple
+
+from rdbmsdiff.schema import DBSchema
 
 
 @dataclass(frozen=True, slots=True)
@@ -101,5 +103,52 @@ class DBTableDiff:
         return self.index_diff.indexes_missing_in_target_db if self.index_diff else _EMPTY_TUPLE
 
  
+class _NamesDiff:
+
+    def __init__(self, source_names: Sequence[str], target_names: Sequence[str]) -> None:
+        self._source_names = set(source_names)
+        self._target_names = set(target_names)
+
+    def names_missing_in_source_db(self) -> Tuple[str, ...]:
+        return tuple(self._target_names - self._source_names)
+
+    def number_of_names_missing_in_source_db(self) -> int:
+        return len(self.names_missing_in_source_db())
+
+    def names_missing_in_target_db(self) -> Tuple[str, ...]:
+        return tuple(self._source_names - self._source_names)
+
+    def number_of_names_missing_in_target_db(self) -> int:
+        return len(self.names_missing_in_target_db())
+
+
 class DBSchemaDiff:
-    ...
+
+    def __init__(self, source_schema: DBSchema, target_schema: DBSchema) -> None:
+        self._sequences_diff = _NamesDiff(source_schema.sequences, target_schema.sequences)
+        self._views_diff = _NamesDiff(source_schema.views, target_schema.views)
+        self._materialized_views_diff = _NamesDiff(source_schema.materialized_views, target_schema.materialized_views)
+
+    def sequences_missing_in_source_db(self) -> Tuple[str, ...]:
+        return self._sequences_diff.names_missing_in_source_db()
+
+    def number_of_sequences_missing_in_source_db(self) -> int:
+        return self._sequences_diff.number_of_names_missing_in_source_db()
+
+    def sequences_missing_in_target_db(self) -> Tuple[str, ...]:
+        return self._sequences_diff.names_missing_in_target_db()
+ 
+    def number_of_sequences_missing_in_target_db(self) -> int:
+        return self._sequences_diff.names_missing_in_target_db()
+
+    def views_missing_in_source_db(self) -> Tuple[str, ...]:
+        return self._views_diff.names_missing_in_source_db()
+
+    def number_of_views_missing_in_source_db(self) -> int:
+        return self._views_diff.number_of_names_missing_in_source_db()
+
+    def views_missing_in_target_db(self) -> Tuple[str, ...]:
+        return self._views_diff.names_missing_in_target_db()
+
+    def number_of_views_missing_in_target_db(self) -> int:
+        return self._views_diff.number_of_names_missing_in_target_db()
