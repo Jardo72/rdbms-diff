b8fdcf99f13668b140dfaee47f72d88dc47d338b
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2024-12-30 20:09:43 +0100

    Just a refactoring

--- rdbmsdiff/schema/report.py
+++ rdbmsdiff/schema/report.py
@@ -6,7 +6,7 @@ from colorama import Fore
 from .diff import DBColumnDiff, DBSchemaDiff, DBTableDiff
 
 
-def _convert_column_diff(column_diff: DBColumnDiff) -> Dict[str, Any]:
+def _generate_column_diff(column_diff: DBColumnDiff) -> Dict[str, Any]:
     return {
         "name": column_diff.name,
         "source_data_type": str(column_diff.source_data_type),
@@ -14,10 +14,10 @@ def _convert_column_diff(column_diff: DBColumnDiff) -> Dict[str, Any]:
     }
 
 
-def _convert_tables_with_incompatible_columns(table_diff_list: Sequence[DBTableDiff]) -> Dict[str, Any]:
+def _generate_tables_with_incompatible_columns(table_diff_list: Sequence[DBTableDiff]) -> Dict[str, Any]:
     result = {}
     for table_diff in table_diff_list:
-        columns_with_distinct_data_type = list(map(_convert_column_diff, table_diff.columns_with_distinct_data_type))
+        columns_with_distinct_data_type = list(map(_generate_column_diff, table_diff.columns_with_distinct_data_type))
         result[table_diff.name] = {
             "columns_missing_in_source_database": table_diff.columns_missing_in_source_db,
             "columns_missing_in_target_database": table_diff.columns_missing_in_target_db,
@@ -26,7 +26,7 @@ def _convert_tables_with_incompatible_columns(table_diff_list: Sequence[DBTableD
     return result
 
  
-def _convert_tables_with_incompatible_constraints(table_diff_list: Sequence[DBTableDiff]) -> Dict[str, Any]:
+def _generate_tables_with_incompatible_constraints(table_diff_list: Sequence[DBTableDiff]) -> Dict[str, Any]:
     result = {}
     for table_diff in table_diff_list:
         result[table_diff.name] = {
@@ -36,7 +36,7 @@ def _convert_tables_with_incompatible_constraints(table_diff_list: Sequence[DBTa
     return result
 
 
-def _convert_tables_with_incompatible_indexes(table_diff_list: Sequence[DBTableDiff]) -> Dict[str, Any]:
+def _generate_tables_with_incompatible_indexes(table_diff_list: Sequence[DBTableDiff]) -> Dict[str, Any]:
     result = {}
     for table_diff in table_diff_list:
         result[table_diff.name] = {
@@ -50,9 +50,9 @@ def write_report(db_schema_diff: DBSchemaDiff, filename: str) -> None:
     report = {
         "tables_missing_in_source_database": db_schema_diff.tables_missing_in_source_db(),
         "tables_missing_in_target_database": db_schema_diff.tables_missing_in_target_db(),
-        "tables_with_distinct_columns": _convert_tables_with_incompatible_columns(db_schema_diff.tables_with_incompatible_columns()),
-        "tables_with_distinct_constraints": _convert_tables_with_incompatible_constraints(db_schema_diff.tables_with_incompatible_constraints()),
-        "tables_with_distinct_indexes": _convert_tables_with_incompatible_indexes(db_schema_diff.tables_with_incompatible_indexes()),
+        "tables_with_distinct_columns": _generate_tables_with_incompatible_columns(db_schema_diff.tables_with_incompatible_columns()),
+        "tables_with_distinct_constraints": _generate_tables_with_incompatible_constraints(db_schema_diff.tables_with_incompatible_constraints()),
+        "tables_with_distinct_indexes": _generate_tables_with_incompatible_indexes(db_schema_diff.tables_with_incompatible_indexes()),
         "sequences_missing_in_source_database": db_schema_diff.sequences_missing_in_source_db(),
         "sequences_missing_in_target_database": db_schema_diff.sequences_missing_in_target_db(),
         "views_missing_in_source_database": db_schema_diff.views_missing_in_source_db(),
