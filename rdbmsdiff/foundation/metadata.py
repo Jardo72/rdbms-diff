6359d963a2f79b7ae00a198fed95c991ee857044
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2025-01-24 18:16:38 +0100

    Common reading of metadata uses rich instead of colorama

--- rdbmsdiff/foundation/metadata.py
+++ rdbmsdiff/foundation/metadata.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Dict, Set, Tuple
 
-from colorama import Fore
+from rich.console import Console
 from sqlalchemy import create_engine, inspect, MetaData
 from sqlalchemy.sql.schema import CheckConstraint, ForeignKeyConstraint, PrimaryKeyConstraint, UniqueConstraint
 
@@ -41,6 +41,14 @@ class DBTable:
     def column_count(self) -> int:
         return len(self.columns)
 
+    @property
+    def constraint_count(self) -> int:
+        return len(self.check_constraints) + len(self.unique_constraints) + len(self.primary_key_constraints) + len(self.foreign_key_constraints)
+
+    @property
+    def index_count(self) -> int:
+        return len(self.indexes)
+
 
 @dataclass(frozen=True, slots=True)
 class DBSchema:
@@ -52,7 +60,7 @@ class DBSchema:
 
 class _MetaDataReader:
 
-    def __init__(self, db_properties: DatabaseProperties) -> None:
+    def __init__(self, db_properties: DatabaseProperties, console: Console) -> None:
         self._db_properties = db_properties
         # TODO
         # it might make sense to promote this class to context manager and move this code
@@ -61,6 +69,7 @@ class _MetaDataReader:
         self._inspection = inspect(self._engine)
         self._meta_data = MetaData(schema=db_properties.schema)
         self._meta_data.reflect(bind=self._engine)
+        self._console = console
 
     def read_meta_data(self) -> DBSchema:
         return DBSchema(
@@ -72,7 +81,7 @@ class _MetaDataReader:
 
     def _read_tables(self) -> Tuple[DBTable, ...]:
         tables = []
-        print(f"{Fore.CYAN}TABLES{Fore.RESET}")
+        self._console.print(f"[cyan]TABLES[/cyan]")
         for name, details in self._meta_data.tables.items():
             if name.startswith(self._db_properties.schema):
                 tokens = name.split(".")
@@ -87,42 +96,43 @@ class _MetaDataReader:
                 indexes=tuple([index.name for index in details.indexes])
             )
             tables.append(table_meta_info)
-            # print(f"{name}: {table_meta_info.column_count} columns/{table_meta_info.constraint_count} constraints/{table_meta_info.index_count} indexes")
-        print(f"Totally {len(tables)} tables")
+            self._console.print(f"{name}: {table_meta_info.column_count} columns/{table_meta_info.constraint_count} constraints/{table_meta_info.index_count} indexes")
+        self._console.print(f"Totally {len(tables)} tables")
         return tuple(tables)
 
     def _read_sequences(self) -> Tuple[str, ...]:
         sequences = []
-        print(f"{Fore.CYAN}SEQUENCES{Fore.RESET}")
+        self._console.print(f"[cyan]SEQUENCES[/cyan]")
         for name in self._inspection.get_sequence_names(self._db_properties.schema):
             print(name)
             sequences.append(name)
-        print(f"Totally {len(sequences)} sequences")
+        self._console.print(f"Totally {len(sequences)} sequences")
         return tuple(sequences)
 
     def _read_views(self) -> Tuple[str, ...]:
         views = []
-        print(f"{Fore.CYAN}VIEWS{Fore.RESET}")
+        self._console.print(f"[cyan]VIEWS[/cyan]")
         for name in self._inspection.get_view_names(self._db_properties.schema):
             print(name)
             views.append(name)
-        print(f"Totally {len(views)} views")
+        self._console.print(f"Totally {len(views)} views")
         return tuple(views)
 
     def _read_materialized_views(self) -> Tuple[str, ...]:
         materialized_views = []
-        print(f"{Fore.CYAN}MATERIALIZED VIEWS{Fore.RESET}")
+        self._console.print(f"[cyan]MATERIALIZED VIEWS[/cyan]")
         for name in self._inspection.get_materialized_view_names(self._db_properties.schema):
             print(name)
             materialized_views.append(name)
-        print(f"Totally {len(materialized_views)} materialized views")
+        self._console.print(f"Totally {len(materialized_views)} materialized views")
         return tuple(materialized_views)
 
 
 def read_db_meta_data(db_properties: DatabaseProperties) -> DBSchema:
-    print()
-    print(f"Going to read meta-info from {Fore.CYAN}{db_properties.url_without_password}{Fore.RESET}, schema {Fore.CYAN}{db_properties.schema}{Fore.RESET}")
+    console = Console(record=False, highlight=False)
+    console.print()
+    console.print(f"Going to read meta-info from [cyan]{db_properties.url_without_password}[/cyan], schema [cyan]{db_properties.schema}[/cyan]")
 
     # TODO: chances are we should take care about closing the reader
-    reader = _MetaDataReader(db_properties)
+    reader = _MetaDataReader(db_properties, console)
     return reader.read_meta_data()
