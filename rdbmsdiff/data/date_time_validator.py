850065c5f5a7427ba35928ea20974987a646fff6
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2025-02-20 20:23:28 +0100

    DateTimeValidator does not depend on SQL functions anymore.

--- rdbmsdiff/data/date_time_validator.py
+++ rdbmsdiff/data/date_time_validator.py
@@ -29,21 +29,10 @@ class DateTimeValidator(AbstractValidator):
     def __init__(self, config: Configuration, table: DBTable, column: DBColumn) -> None:
         super().__init__(config, table, column)
 
-    def _get_format_pattern(self) -> str:
-        if self.column.is_date:
-            return "YYYY-MM-DD"
-        elif self.column.is_time:
-            return "HH24:MI:SS"
-        elif self.column.is_timestamp:
-            return "YYYY-MM-DD HH24:MI:SS"
-        else:
-            raise ValueError(f"Unexpected column type: {self._column_type}")
-
     def _select(self, db_properties: DatabaseProperties) -> ValidationQuery:
         engine = self.create_engine(db_properties)
         with Session(engine) as session:
-            format_pattern = self._get_format_pattern()
-            statement = f"SELECT TO_CHAR({self.column_name}, '{format_pattern}') FROM {self.table_name} WHERE {self.column_name} IS NOT NULL ORDER BY TO_CHAR({self.column_name}, '{format_pattern}') ASC LIMIT {self.limit}"
+            statement = f"SELECT {self.column_name} FROM {self.table_name} WHERE {self.column_name} IS NOT NULL ORDER BY {self.column_name} ASC LIMIT {self.limit}"
             result = session.execute(text(statement)).all()
             return ValidationQuery(
                 sql=statement,
