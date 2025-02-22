a37005ce77949a07030a14530dcf92d3cf949a64
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2025-02-22 20:11:08 +0100

    Stopwatch added to foundation package.

--- rdbmsdiff/foundation/__init__.py
+++ rdbmsdiff/foundation/__init__.py
@@ -20,4 +20,5 @@ from .config import Configuration, DatabaseProperties, ReadConfigurationError
 from .config import epilog, handle_configuration_error, read_config
 from .metadata import DBColumn, DBTable, DBSchema
 from .metadata import read_db_meta_data
+from .stopwatch import Stopwatch
 from .util import Status
